e625dfde89b8feafd372deedc09b15520b88aede
Jefferson Lima <jeffyuri@hotmail.com>
2023-08-14 09:02:01 -0300

    Novo módulo para exceções personalizadas

---
+++ src/tratamento_erros.js
@@ -0,0 +1,9 @@
+// Uma classe para tratar erros personalizados.
+class MetodoPagamentoError extends Error {
+  constructor(message) {
+    super(message)
+    this.name = "MetodoPagamentoError";
+  }
+}
+
+export default MetodoPagamentoError;
