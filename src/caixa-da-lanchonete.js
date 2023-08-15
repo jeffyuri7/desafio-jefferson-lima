bc30fb5171254c12433310797b3a920f6ffb0501
Jefferson Lima <jeffyuri@hotmail.com>
2023-08-15 09:26:24 -0300

    Implementa a função calcularValorDaCompra.

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,9 +1,22 @@
+import Pedido from './pedido.js';
+
 class CaixaDaLanchonete {
 
     calcularValorDaCompra(metodoDePagamento, itens) {
-        return "";
+        try {
+            const pedido = new Pedido(metodoDePagamento, itens);
+            const valor = pedido.calcularPedido();
+            return valor;
+        } catch (erro) {
+            return erro.message;
+        }
     }
 
 }
+let caixa = new CaixaDaLanchonete();
+let itens = ['cafe,1', 'chantily,3']
+let resultado = caixa.calcularValorDaCompra("credito", itens);
+console.log(typeof (resultado))
+console.log(resultado);
 
 export { CaixaDaLanchonete };
