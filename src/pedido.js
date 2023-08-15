c2728b09d93a458a3155c44a555fb5569aba782b
Jefferson Lima <jeffyuri@hotmail.com>
2023-08-15 13:37:56 -0300

    Import e lançamento de FaltaItensCarrinhoError.

--- src/pedido.js
+++ src/pedido.js
@@ -2,6 +2,8 @@ import Cardapio from './cardapio.js';
 import Pagamento from './pagamento.js';
 import FaltaDePedidoExtraError from './erros/falta-pedido-extra-error.js';
 import QuantidadeInvalidaError from './erros/quantidade-error.js';
+import FaltaItensCarrinhoError from './erros/falta-itens-error.js';
+import ItemInvalidoError from './erros/item-invalido.js';
 
 // Cria um novo pedido.
 class Pedido {
@@ -17,20 +19,32 @@ class Pedido {
     let listaFinal = [];
     let itensPedido = this.itens;
     let listaDeItens = this.cardapio.itens;
+
+    // Verifica se há algum item no pedido.
+    if (itensPedido == false) {
+      throw new FaltaItensCarrinhoError("Não há itens no carrinho de compra!");
+    }
+
     // Transforma os itens em um array.
     itensPedido.forEach(function (item) {
       let codigo = item.split(",");
       listaItensPedido.push(codigo)
     });
+
+    console.log(listaItensPedido)
+
     // Compara os itens do pedido com o cardápio
-    // TODO Lógica de conferência se o item é válido.
     listaItensPedido.forEach(function (item) {
       listaDeItens.forEach(function (itemDoCardapio) {
-        if (item[1] == 0) {
+        // Verifica se a quantidade é maior ou igual a 1.
+        if (item[1] <= 0) {
           throw new QuantidadeInvalidaError("Quantidade inválida!");
         }
+        // Compara o item com o cardápio.
         if (item[0] === itemDoCardapio[0]) {
+          // Se o item é adicional.
           if (itemDoCardapio[3]) {
+            // Verifica se o item principal está no pedido.
             if (!(listaItensPedido.find(cod => cod[0] === itemDoCardapio[3]))) {
               throw new FaltaDePedidoExtraError("Item extra não pode ser pedido sem o principal");
             }
