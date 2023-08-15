e43df27de689228bd8e342e8209abb7ccd0c67d6
Jefferson Lima <jeffyuri@hotmail.com>
2023-08-14 21:32:05 -0300

    Renomeia o modulo de tratamento de erros.

--- src/erros/metodo-pagamento.js
+++ src/erros/metodo-pagamento.js
@@ -1,9 +1,10 @@
 // Uma classe para tratar erros personalizados.
 class MetodoPagamentoError extends Error {
   constructor(message) {
-    super(message)
+    super(message);
     this.name = "MetodoPagamentoError";
   }
 }
+
 
 export default MetodoPagamentoError;
