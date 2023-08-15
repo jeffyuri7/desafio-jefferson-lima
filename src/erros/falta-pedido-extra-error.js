776f7159b0a66fec17cee4740774d27047008a49
Jefferson Lima <jeffyuri@hotmail.com>
2023-08-14 21:33:43 -0300

    Cria FaltaDePedidoExtraError.

---
+++ src/erros/falta-pedido-extra-error.js
@@ -0,0 +1,9 @@
+class FaltaDePedidoExtraError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "FaltaDePedidoExtraError ";
+  }
+}
+
+
+export default FaltaDePedidoExtraError;
