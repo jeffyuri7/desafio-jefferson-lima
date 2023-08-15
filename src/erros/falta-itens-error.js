631cd50cbbd6e3ba935564acc9c98886ee1be3cf
Jefferson Lima <jeffyuri@hotmail.com>
2023-08-15 13:36:32 -0300

    Cria o módulo FaltaItensCarrinhoError.

---
+++ src/erros/falta-itens-error.js
@@ -0,0 +1,10 @@
+// Uma classe para tratar erros personalizados.
+class FaltaItensCarrinhoError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "FaltaItensCarrinhoError";
+  }
+}
+
+
+export default FaltaItensCarrinhoError;
